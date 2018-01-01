dabcc19f95e6dbdf4582f4ab7cef8fc5148699a8
Marko <markthomasnoonan@gmail.com>
2018-01-01 16:34:47 -0500

    move config to seperate file

--- src/main.js
+++ src/main.js
@@ -4,19 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import firebase from 'firebase'
-
+import config from './firebase-config'
 Vue.config.productionTip = false
 
 let app
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCX5U04FhH0GofSTSi8sx6y1UjAbn72gyI",
-    authDomain: "folkmusic-dashboard.firebaseapp.com",
-    databaseURL: "https://folkmusic-dashboard.firebaseio.com",
-    projectId: "folkmusic-dashboard",
-    storageBucket: "",
-    messagingSenderId: "759403886875"
-  };
 firebase.initializeApp(config)
   
 firebase.auth().onAuthStateChanged(function (user) {
