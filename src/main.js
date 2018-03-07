3dff01723658ce2c98acc97fe0653733075ee3cc
PrettierCI <commits@prettierci.com>
2018-03-07 00:21:36 +0000

    Sync with Prettier

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import firebase from 'firebase';
-import config from './firebase-config';
-import UserStore from './stores/UserStore';
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import firebase from 'firebase'
+import config from './firebase-config'
+import UserStore from './stores/UserStore'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-let app;
+let app
 // Initialize Firebase
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
 firebase.auth().onAuthStateChanged(function(user) {
 	if (!app) {
@@ -20,7 +20,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 			router,
 			template: '<App/>',
 			components: {App}
-		});
+		})
 	}
 	if (user) {
 		firebase
@@ -29,12 +29,12 @@ firebase.auth().onAuthStateChanged(function(user) {
 			.once('value', function(nameSnap) {
 				// if they set a name when signing up, we can greet them with it,
 				// otherwise, we use their email address.
-				UserStore.set('username', nameSnap.val() || user.email);
-			});
+				UserStore.set('username', nameSnap.val() || user.email)
+			})
 	}
 	if (!user) {
-		UserStore.set('username', '');
+		UserStore.set('username', '')
 	}
-});
+})
 
 /* eslint-disable no-new */
